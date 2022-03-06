5fbc93b189092e7df8dca305a8caf16bd6c77b1f
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-06 21:22:04 +0300

    add validate email method

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,9 +1,22 @@
 package users
 
+import (
+	"github/atakanteko/bookstore_users-api/utils/errors"
+	"strings"
+)
+
 type User struct {
 	ID          int    `json:"id"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(user.Email)
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
 }
