7b3a37108f4d59720e8fd3375c5e63880890ceec
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-06 20:25:42 +0300

    scaffold create user function

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github/atakanteko/bookstore_users-api/domain/users"
+	"github/atakanteko/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
