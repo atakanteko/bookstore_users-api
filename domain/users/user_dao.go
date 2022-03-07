615f75b4179d81bb769ea89a9659d29a3e0cef81
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-08 01:16:20 +0300

    scaffold user dao

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,1 +1,36 @@
 package users
+
+import (
+	"fmt"
+	"github/atakanteko/bookstore_users-api/utils/errors"
+)
+
+var (
+	userDB = make(map[int]*User)
+)
+
+func (user *User) Get() *errors.RestErr {
+	result := userDB[user.ID]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.ID))
+	}
+	user.ID = result.ID
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+func (user *User) Save() *errors.RestErr {
+	result := userDB[user.ID]
+	if result == nil {
+		if result.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("user %s already registered", user.Email))
+
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.ID))
+	}
+	userDB[user.ID] = user
+	return nil
+}
