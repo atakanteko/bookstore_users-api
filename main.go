8770e478608523fb72a8bc64880685aacdc27c79
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-10 01:34:13 +0300

    get env

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github/atakanteko/bookstore_users-api/app"
+import (
+	"github.com/joho/godotenv"
+	"github/atakanteko/bookstore_users-api/app"
+	"log"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	app.StartApplication()
 }
